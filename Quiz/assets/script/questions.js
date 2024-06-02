8388180636b9084ac2981fb36ca97db7ca51b488
landim9 <landimr9des@gmail.com>
2024-06-02 20:08:44 -0300

    senai

---
+++ Quiz/assets/script/questions.js
@@ -0,0 +1,100 @@
+export default [
+    {
+        question: "Qual seleção venceu a Copa do Mundo de 2018?",
+        answers: [
+            {option: "Argentina", correct:false},
+            {option: "Brasil", correct:false},
+            {option: "França", correct:true},
+        ],
+    },
+
+    {
+        question: "Qual jogador tem o recorde de mais gols marcados na história da Copa do Mundo?",
+        answers: [
+            {option: "Miroslav Klose", correct:true},
+            {option: "Pelé", correct:false},
+            {option: "Cristiano Ronaldo", correct:false},
+        ],
+    },
+    
+    {
+        question: "Em que país nasceu o futebol moderno?",
+        answers: [
+            {option: "Brasil", correct:false},
+            {option: "Inglaterra", correct:true},
+            {option: "Itália", correct:false},
+        ],
+        
+    },
+
+    {
+        question: "Qual clube ganhou a UEFA Champions League mais vezes?",
+        answers: [
+            {option: "Barcelona", correct:false},
+            {option: "Real Madrid", correct:true},
+            {option: "Bayern de Munique", correct:false},
+        ],
+        
+    },
+    
+    {
+        question: "Quem ganhou a Bola de Ouro FIFA em 2020?",
+        answers: [
+            {option: "Lionel Messi", correct:false},
+            {option: "Cristiano Ronaldo", correct:false},
+            {option: "Robert Lewandowski", correct:true},
+
+        ],
+        
+    },
+
+    {
+        question: "Qual país venceu a Copa do Mundo de 2014?",
+        answers: [
+            {option: "Alemanha", correct:true},
+            {option: "Espanha", correct:false},
+            {option: "Argentina", correct:false},
+        ],
+        
+    },
+
+    {
+        question: "Qual é o nome do estádio do Barcelona?",
+        answers: [
+            {option: "Santiago Bernabéu", correct:false},
+            {option: "Camp Nou", correct:true},
+            {option: "Anfield", correct:false},
+        ],
+        
+    },
+
+    {
+        question: "Qual jogador é conhecido como 'O Fenômeno'?",
+        answers: [
+            {option: "Ronaldo Nazário", correct:true},
+            {option: "Ronaldinho Gaúcho", correct:false},
+            {option: "Neymar", correct:false},
+        ],
+        
+    },
+
+    {
+        question: "Quem é o técnico da seleção brasileira de futebol masculino?",
+        answers: [
+            {option: "Dorival Jr", correct:true},
+            {option: "Tite", correct:false},
+            {option: "Fernando Diniz", correct:false},
+        ],
+        
+    },
+
+    {
+        question: "Qual clube ganhou mais títulos da Premier League inglesa?",
+        answers: [
+            {option: "Manchester United", correct:true},
+            {option: "Manchester City", correct:false},
+            {option: "Chelsea", correct:false},
+        ],
+        
+    },
+];
